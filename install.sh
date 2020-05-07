ad1e7a583d9b3694923f251657d459ad27704a3a
Anton Skochko <antonskochko@gmail.com>
2020-05-07 14:45:30 +0300

    added missing changes

--- install.sh
+++ install.sh
@@ -9,6 +9,12 @@ if [ $SHELL != "/bin/zsh" ]; then chsh -s $(which zsh) $(whoami) ; fi
 # installing oh-my-zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ; fi
 
+if [ ! -f "$HOME/.vim/autoload/plug.vim" ]
+then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
 # copying config files into $HOME
 FILES="$(find .  -maxdepth 1 -type f -name '*' -a ! -name 'install.sh')"
 cp $FILES $HOME/
