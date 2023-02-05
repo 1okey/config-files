9aead025fc0f86db2db8b5a707e50ed9b2e4cb96
1okey <antonskochko@gmail.com>
2023-02-05 18:56:37 +0000

    add tpm, rust

--- install.ubuntu.sh
+++ install.ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -z $(uname -a | grep -oiE '(Ubuntu|microsoft)') ]
+if [ -z $(uname -a | grep -oiE '(Ubuntu|microsoft|raspberry)') ]
 then 
     echo 'This script meant to run only on Ubuntu distros'
     exit
@@ -12,7 +12,13 @@ apt install git zsh tmux vim golang -y
 # changing default shell
 if [ $SHELL != "/bin/zsh" ]
 then 
-    chsh -s $(which zsh) $(whoami)
+	if [ ! -z $(cat /etc/shells | grep -E '/bin.zsh') ]
+	then
+		echo "Installing /bin/zsh"
+		apt-get install zsh
+	fi
+	echo "Changing $SHELL to /bin/zsh"
+	chsh -s $(which zsh) $(whoami)
 fi
 
 # installing oh-my-zsh
@@ -27,7 +33,24 @@ then
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
+
+# installing rust:
+if [ -z "$(rustup --version 2> /dev/null)" ];
+then
+    echo "Installing rust..."		
+	curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
+fi
+
+
+# installint tpm:
+if [ ! -d "$HOME/.tmux/plugins/tpm" ];
+then
+	echo "Installing tpm..."
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+
 # copying config files into $HOME
 FILES="$(find .  -maxdepth 1 -type f -name '*' -a ! -name 'install.*.sh' ! -name '*.md')"
 cp $FILES $HOME/
-rm ~/.bash*
+rm ~/.bash* 2> /dev/null
