71cb8ef16c1bffe638875193c0764e60293312e9
1okey <antonskochko@gmail.com>
2024-03-08 21:52:30 +0200

    macos installer update

--- .zshrc
+++ .zshrc
@@ -2,12 +2,12 @@ if [ -d "$HOME/bin" ]; then PATH="$HOME/bin:$PATH" ; fi
 if [ -d "$HOME/.local/bin" ]; then PATH="$HOME/.local/bin:$PATH" ; fi
 if [ -d "$HOME/snap" ]; then PATH="$HOME/snap:$PATH" ; fi
 if [ -d "/snap" ]; then PATH="/snap/bin:$PATH"; fi
+if [ -d "/usr/local/go" ]; then PATH="/usr/local/go/bin:$PATH"; fi
 
 if [ -f "$HOME/.aliases" ]; then . $HOME/.aliases ; fi
 if [ -f "$HOME/.env" ]; then . $HOME/.env ; fi
 
 export ZSH="$HOME/.oh-my-zsh"
-export GOPATH="$HOME/go"
 
 plugins=(git)
 
--- install.macos.sh
+++ install.macos.sh
@@ -18,29 +18,58 @@ function set_macos_defaults() {
 }
 
 function install() {
-    # install vim if missing
-    [ ! -x $(command -v vim) ] && brew install vim
-    cp .vimrc ~/.vimrc
+    if [ $SHELL !=  "/bin/zsh" ]
+    then
+	chsh -s /bin/zsh	
+    fi
+
+    if [[ ! -x $(which nvm) ]]
+    then
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+        source ./.env
+
+        nvm install node
+    fi
 
     if [[ ! -x $(which brew) ]]
     then
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
 
-    if [ ! -f "$HOME/.vim/autoload/plug.vim" ]
-    then
-        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    # install alacrittty if missing
+    ! command -v alacritty > /dev/null && brew install --cask alacritty
+
+    # install neovim if missing
+    ! command -v nvim > /dev/null && brew install neovim
+
+    # install neovim if missing
+    ! command -v tmux > /dev/null && brew install tmux
+
+    # installing tpm:
+    if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+        echo "Installing tpm..."
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     fi
+
+    if [ ! -d "$HOME/.config" ]
+    then
+	mkdir "$HOME/.config"	
+    fi
+
+    cp -r ./nvim "$HOME/.config/"
 
     if [ ! -d "$HOME/.oh-my-zsh" ]
     then
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     fi
 
     # copying config files into $HOME
     FILES="$(find .  -maxdepth 1 -type f -name '*' -a ! -name 'install.*.sh' ! -name '*.md')"
+
     cp $FILES $HOME/
+
+    rm ~/.bash* 2> /dev/null
 }
 
 if [[ $1 == '--sync' ]]
