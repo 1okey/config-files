de2e52cf64d0006ad0cc45a5eeb59cdeca2635d2
1okey <antonskochko@gmail.com>
2024-11-09 22:56:17 +0200

    update reload command so that the cache for user modules is busted

--- nvim/lua/user/commands.lua
+++ nvim/lua/user/commands.lua
@@ -1,1 +1,11 @@
-vim.api.nvim_create_user_command('Reload', 'so $MYVIMRC', {})
+vim.api.nvim_create_user_command('Reload', function()
+  dofile(vim.env.MYVIMRC)
+
+  vim.print("Reloading user configs")
+  for name,_ in pairs(package.loaded) do
+    if name:match('^user') then
+      package.loaded[name] = nil
+      require(name)
+    end
+  end
+end , {})
